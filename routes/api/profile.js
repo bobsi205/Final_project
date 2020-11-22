19d9c4a09a7ea65b25f5bc34c9348901ae33d1a8
bobsi205 <47793000+bobsi205@users.noreply.github.com>
2020-11-22 14:07:27 +0200

    readded request library added get profile by user id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -66,54 +66,29 @@ router.post('/', auth, async (req, res) => {
   }
 });
 
-// // @route   GET api/profile
-// // @desc    Get all profiles
-// // @access  public
-// router.get('/', async (req, res) => {
-//   try {
-//     const profiles = await Profile.find().populate('user', ['name', 'avatar']);
-//     res.json(profiles);
-//   } catch (error) {
-//     console.error(error.message);
-//     res.status(500).send('Server Error');
-//   }
-// });
-
-// // @route   GET api/profile/user/:user_id
-// // @desc    Get profile by user ID
-// // @access  public
-// router.get('/user/:user_id', async (req, res) => {
-//   try {
-//     const profile = await Profile.findOne({
-//       user: req.params.user_id,
-//     }).populate('user', ['name', 'avatar']);
-//     if (!profile) return res.status(400).json({ msg: 'profile not found' });
-//     res.json(profile);
-//   } catch (err) {
-//     console.error(err.message);
-//     if (err.kind == 'ObjectId') {
-//       return res.status(400).json({ msg: 'profile not found' });
-//     }
-//     res.status(500).send('Server Error');
-//   }
-// });
-
-// // @route   DELETE api/profile
-// // @desc    Delete profile, user & posts
-// // @access  Private
-// router.delete('/', auth, async (req, res) => {
-//   try {
-//     //remove profile
-//     await Profile.findOneAndRemove({ user: req.user.id });
-//     //remove user
-//     await User.findOneAndRemove({ _id: req.user.id });
-
-//     res.json({ msg: 'user deleted' });
-//   } catch (error) {
-//     console.error(error.message);
-//     res.status(500).send('Server Error');
-//   }
-// });
+// @route   GET api/profile/user/:user_id
+// @desc    Get profile by user ID
+// @access  public
+router.get('/user/:user_id', async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id,
+    }).populate('user', [
+      'firstName',
+      'lastName',
+      'picture',
+      'uploadedSummaries',
+    ]);
+    if (!profile) return res.status(400).json({ msg: 'profile not found' });
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind == 'ObjectId') {
+      return res.status(400).json({ msg: 'profile not found' });
+    }
+    res.status(500).send('Server Error');
+  }
+});
 
 // @route   PUT api/profile/education
 // @desc    Add profile education
