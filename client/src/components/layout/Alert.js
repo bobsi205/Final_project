a4b8894c92504e79026a529b86b24944592a300e
bobsi205 <47793000+bobsi205@users.noreply.github.com>
2020-11-23 19:50:28 +0200

    alert component

---
+++ client/src/components/layout/Alert.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Alert } from 'react-bootstrap';
+
+const Alert = ({ alerts }) =>
+  alerts.map((alert) => (
+    <Alert key={alert.id} variant={alert.alertType}>
+      {alert.msg}
+    </Alert>
+  ));
+
+Alert.propTypes = {
+  alerts: PropTypes.array.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  alerts: state.alert,
+});
+
+export default connect(mapStateToProps)(Alert);
