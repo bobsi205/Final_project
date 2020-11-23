468c6496eca4f4020c84f42ecd0f2063760a14fb
bobsi205 <47793000+bobsi205@users.noreply.github.com>
2020-11-23 21:53:13 +0200

    renamed nabar file to navigation

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Navbar from './components/layout/Navbar';
+import Navbar from './components/layout/Navigation';
 import Home from './components/home/Home';
 import Routes from './components/routing/Routes';
 import { LOGOUT } from './actions/types';
