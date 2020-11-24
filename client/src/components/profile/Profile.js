e91dc0edb88a40992be25ddeed87fd3b1822791a
bobsi205 <47793000+bobsi205@users.noreply.github.com>
2020-11-24 15:03:33 +0200

    added profile component and routing

---
+++ client/src/components/profile/Profile.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Profile = (props) => {
+  return (
+    <div>
+      <p>this is profile</p>
+    </div>
+  );
+};
+
+Profile.propTypes = {};
+
+export default Profile;
