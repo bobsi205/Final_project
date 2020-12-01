31e7c07ca3d00b902cc04f08995d771d318fd678
bobsi205 <47793000+bobsi205@users.noreply.github.com>
2020-12-01 15:12:33 +0200

    homepage css

--- client/src/components/home/CategoryBox.js
+++ client/src/components/home/CategoryBox.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
+import '../../style/style.css';
+
 export const CategoryBox = (props) => {
   return (
     <Button
+      className="zoom zoom:hover"
       size="lg"
       style={{
         height: '180px',
         width: '100%',
         display: 'block',
+        color: 'white',
+        fontSize: '2rem',
+        textShadow: '1px 2px black',
         borderRadius: '0',
         border: 'solid white 1px',
-        backgroundImage: 'url(cat/accounting.jpg)',
+        backgroundImage: `url(${props.image})`,
         backgroundSize: 'cover',
+        overflow: 'hidden',
       }}
     >
       {props.cat}
--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -50,50 +50,51 @@ export const Home = () => {
   ]);
 
   return (
-    <Container>
+    <>
       <Image src="banner.jpg" className="img-fluid mb-3" />
+      <Container>
+        <Row className="my-3">
+          <h2 className="ml-4 mt-2 text-primary">Recent</h2>
+          <Container style={{ overflow: 'hidden' }}>
+            <HomeDeck cards={data} />
+          </Container>
+        </Row>
+        <hr />
+        <Row>
+          {catagoriesDB.map((cat) => {
+            return (
+              <Col xs={6} md={3} className="p-0">
+                <CategoryBox cat={cat.name} image={cat.img} />
+              </Col>
+            );
+          })}
+        </Row>
+        <hr />
 
-      <Row className="my-3">
-        <h2 className="ml-4 mt-2 text-primary">Recent</h2>
-        <Container style={{ overflow: 'hidden' }}>
-          <HomeDeck cards={data} />
-        </Container>
-      </Row>
-      <hr />
-      <Row>
-        {catagoriesDB.map((cat) => {
-          return (
-            <Col xs={6} md={3} className="p-0">
-              <CategoryBox cat={cat.name} color={cat.color} />
-            </Col>
-          );
-        })}
-      </Row>
-      <hr />
+        <Row className="my-3">
+          <h2 className="ml-4 mt-2 text-primary">Recommended</h2>
+          <Container style={{ overflow: 'hidden' }}>
+            <HomeDeck cards={data} />
+          </Container>
+        </Row>
+        <hr />
 
-      <Row className="my-3">
-        <h2 className="ml-4 mt-2 text-primary">Recommended</h2>
-        <Container style={{ overflow: 'hidden' }}>
-          <HomeDeck cards={data} />
-        </Container>
-      </Row>
-      <hr />
+        <Row className="my-3">
+          <h2 className="ml-4 mt-2 text-primary">Popular</h2>
+          <Container style={{ overflow: 'hidden' }}>
+            <HomeDeck cards={data} />
+          </Container>
+        </Row>
+        <hr />
 
-      <Row className="my-3">
-        <h2 className="ml-4 mt-2 text-primary">Popular</h2>
-        <Container style={{ overflow: 'hidden' }}>
-          <HomeDeck cards={data} />
-        </Container>
-      </Row>
-      <hr />
-
-      <Row className="my-3">
-        <h2 className="ml-4 mt-2 text-primary">New</h2>
-        <Container style={{ overflow: 'hidden' }}>
-          <HomeDeck cards={data} />
-        </Container>
-      </Row>
-    </Container>
+        <Row className="my-3">
+          <h2 className="ml-4 mt-2 text-primary">New</h2>
+          <Container style={{ overflow: 'hidden' }}>
+            <HomeDeck cards={data} />
+          </Container>
+        </Row>
+      </Container>
+    </>
   );
 };
 
