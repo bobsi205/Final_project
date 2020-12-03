834d45e3ad9ddd42c9d8373dcf706fcac53d8f82
bobsi205 <47793000+bobsi205@users.noreply.github.com>
2020-12-03 15:08:37 +0200

    404 page

--- client/src/components/layout/NotFound.js
+++ client/src/components/layout/NotFound.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import { Container, Image } from 'react-bootstrap';
 
 const NotFound = () => {
   return (
-    <>
-      <h1 className="x-large text-primary">
-        <i className="fas fa-exclamation-triangle" /> Page Not Found
-      </h1>
-      <p className="large">Sorry, this page does not exist</p>
-    </>
+    <Container className="text-center mt-5">
+      <h1 className="display-4">We are sorry,</h1>
+      <p className="text-muted">
+        but this page you were looking for can't found
+      </p>
+      <Image className="w-75" src="404-page.png" alt="404page" />
+    </Container>
   );
 };
 
