81e1cbd1527f8fe163dada6bccfe5bc32cd50e69
bobsi205 <47793000+bobsi205@users.noreply.github.com>
2020-12-07 17:26:34 +0200

    added redux to summary

--- client/src/components/summary/Summary.js
+++ client/src/components/summary/Summary.js
@@ -1,25 +1,29 @@
-import React from "react";
-import { Card, Container, Button, Image } from "react-bootstrap";
-import Rating from "./Rating"
-export default function Summary() {
-    
-  const newSummary = {
-    text: "text",
-    firstName: "first Name",
-    lastName: "last Name",
-    picture: "",
-    title: "title",
-    category: "category",
-    user: "",
-    rate:3
-  };
+import React from 'react';
+import { Card, Container, Button, Image } from 'react-bootstrap';
+import Rating from './Rating';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
+export const Summary = ({ auth }) => {
+  const newSummary = {
+    text: 'text',
+    firstName: 'first Name',
+    lastName: 'last Name',
+    picture: '',
+    title: 'title',
+    category: 'category',
+    user: '',
+    rate: 3,
+  };
+  React.useEffect(() => {
+    console.log(auth);
+  });
   return (
     <div>
       <Container>
         <Card>
           <Card.Header>
-              <Rating rate={newSummary.rate}/>
+            <Rating rate={newSummary.rate} />
             <Image variant="top" src="*" />
           </Card.Header>
           <Card.Body>
@@ -31,10 +35,20 @@ export default function Summary() {
             <Button variant="primary">Go somewhere</Button>
           </Card.Body>
           <Card.Footer>
-              <Card.Text>dasdasdadasds</Card.Text>
+            <Card.Text>dasdasdadasds</Card.Text>
           </Card.Footer>
         </Card>
       </Container>
     </div>
   );
-}
+};
+
+Summary.propTypes = {
+  auth: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+});
+
+export default connect(mapStateToProps, null)(Summary);
