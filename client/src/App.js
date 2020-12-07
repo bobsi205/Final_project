fb1be540672d526e40650611ab960721b450ae45
bobsi205 <47793000+bobsi205@users.noreply.github.com>
2020-12-07 16:10:15 +0200

    fixed routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,20 @@
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/layout/navigation/Navigation';
+import Footer from './components/layout/footer/Footer';
+import Home from './components/home/Home';
+import Routes from './components/routing/Routes';
+import About from './components/layout/footer/About';
+import FAQ from './components/layout/footer/Faq';
+import Summary from './components/summary/Summary';
 
-import React, { useEffect } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Navbar from "./components/layout/navigation/Navigation";
-import Footer from "./components/layout/footer/Footer";
-import Home from "./components/home/Home";
-import Routes from "./components/Routing/Routes";
-import About from "./components/layout/footer/About"
-import FAQ from "./components/layout/footer/Faq"
-import Summary from "./components/summary/Summary"
-
-import { LOGOUT } from "./actions/types";
+import { LOGOUT } from './actions/types';
 
 // Redux
-import { Provider } from "react-redux";
-import store from "./store";
-import { loadUser } from "./actions/auth";
-import setAuthToken from "./utils/setAuthToken";
+import { Provider } from 'react-redux';
+import store from './store';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
 
 const App = () => {
   useEffect(() => {
@@ -26,7 +25,7 @@ const App = () => {
     store.dispatch(loadUser());
 
     // log user out from all tabs if they log out in one tab
-    window.addEventListener("storage", () => {
+    window.addEventListener('storage', () => {
       if (!localStorage.token) store.dispatch({ type: LOGOUT });
     });
   }, []);
