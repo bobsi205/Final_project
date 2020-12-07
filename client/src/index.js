4af7941d239dc8b1f2003f883a6054448579b09b
bobsi205 <47793000+bobsi205@users.noreply.github.com>
2020-12-07 16:12:04 +0200

    import fix

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './Style/bootstrap.min.css';
-import './Style/style.css'
+import './style/bootstrap.min.css';
+import './style/style.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
