7d2c3adc17b6d793ad66234378a3dc92befb0532
bobsi205 <47793000+bobsi205@users.noreply.github.com>
2020-12-12 23:18:30 +0200

    404 typo and removed facebook and google login

--- client/src/components/layout/NotFound.js
+++ client/src/components/layout/NotFound.js
@@ -6,7 +6,7 @@ const NotFound = () => {
     <Container className="text-center mt-5">
       <h1 className="display-4">We are sorry,</h1>
       <p className="text-muted">
-        but this page you were looking for can't found
+        but this page you were looking for can not be found
       </p>
       <Image className="w-75" src="/404-page.png" alt="404page" />
     </Container>
