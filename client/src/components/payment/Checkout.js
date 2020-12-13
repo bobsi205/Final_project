91c819763e13c98614f553bca75b93aefc38bffe
bobsi205 <47793000+bobsi205@users.noreply.github.com>
2020-12-13 12:58:12 +0200

    checkout working

--- client/src/components/payment/Checkout.js
+++ client/src/components/payment/Checkout.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Button, Container } from 'react-bootstrap';
+import { Button, Card, Container } from 'react-bootstrap';
 import PayPal from './PayPal';
 
 export default function Checkout() {
@@ -19,7 +19,43 @@ export default function Checkout() {
           <Card.Title>
             <p className="">How many coins would tou like to purchase? </p>
             <p className="">5$ = 25 points</p>
-
+          </Card.Title>
+        </Card.Header>
+        <Card.Body>
+          <div className="d-flex justify-content-center m-2 px-auto w-auto">
+            <input
+              value={amount}
+              onChange={(e) => onHandleChange(e)}
+              type="range"
+              className="form-control-range m-2"
+              min="5"
+              max="100"
+              step="5"
+            ></input>
+            <Button className="m-2 rounded-pill">{amount}$</Button>
+            <Card.Title className="m-2 rounded-pill">
+              {amount * 5} point
+            </Card.Title>
+          </div>
+        </Card.Body>
+        <Card.Footer>
+          <Button
+            className="m-2"
+            onClick={() => {
+              setCheckout(true);
+            }}
+          >
+            Checkout
+          </Button>
+          {checkout ? (
+            <div className="invisible">
+              <PayPal amount={amount} />
+            </div>
+          ) : (
+            ''
+          )}
+        </Card.Footer>
+      </Card>
       <Button
         className="m-2"
         onClick={() => {
