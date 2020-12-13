d9843dba1495e65499ea17faa17bef6a0ccd0c3b
lilachKAT <lilachkatzabi@gmail.com>
2020-12-13 17:30:49 +0200

    rating display function

--- client/src/components/rating/Rating.js
+++ client/src/components/rating/Rating.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import StarFull from './images/star_full.svg';
 import StarEmpty from './images/star_empty.svg';
+import HalfStar from './images/star-half-empty.svg';
+
 import { Image } from 'react-bootstrap';
 
 export default function Rating(props) {
   const star = [];
-
+  let b = true;
   for (let i = 0; i < 5; i++) {
-    i < props.rate ? star.push(StarFull) : star.push(StarEmpty);
+    if (i < Math.floor(props.rate)) star.push(StarFull);
   }
+  for (let i = star.length; i < 5; i++) {
+    if (props.rate - Math.floor(props.rate) && b) {
+      star.push(HalfStar);
+      b = false;
+    } else {
+      star.push(StarEmpty);
+    }
+  }
+
   return (
     <div className="d-flex m-2">
       {star.map((s) => (
