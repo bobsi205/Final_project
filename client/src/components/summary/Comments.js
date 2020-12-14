b8ad3d3fefb41c560b7284bcc63d749257c54a09
bobsi205 <47793000+bobsi205@users.noreply.github.com>
2020-12-14 03:42:14 +0200

    fixed summary img ratio in comments

--- client/src/components/summary/Comments.js
+++ client/src/components/summary/Comments.js
@@ -4,7 +4,12 @@ import { Media, Image } from 'react-bootstrap';
 export const Comments = ({ comment }) => {
   return (
     <Media className="my-2" key={comment._id}>
-      <Image className="rounded-circle m-2" src={comment.picture} height="58" />
+      <Image
+        className="rounded-circle m-2"
+        src={comment.picture}
+        height="58"
+        width="58"
+      />
       <Media.Body
         className="py-2 px-4"
         style={{ backgroundColor: '#F1F1F1', borderRadius: '1rem' }}
